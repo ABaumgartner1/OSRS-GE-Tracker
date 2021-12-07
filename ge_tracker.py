d976d190b12a713d03360c6efeed011bceda7786
ABaumgartner1 <63395475+ABaumgartner1@users.noreply.github.com>
2021-12-07 01:21:34 -0800

    Update ge_tracker.py
    added some comments

--- ge_tracker.py
+++ ge_tracker.py
@@ -1,13 +1,9 @@
-# python web scraping notes
-# - pip install requests
-# - pip install lxml
-# - pip install bs4
-
-# osrs ge item scraper
+# OSRS GE item scraper created by Aaron Baumgartner in 2021
 
 import requests
 import bs4
 
+# hold the current urls for all items I want to track
 item_urls = ["https://secure.runescape.com/m=itemdb_oldschool/Dragon+hunter+lance/viewitem?obj=22978", "https://secure.runescape.com/m=itemdb_oldschool/Bandos+chestplate/viewitem?obj=11832", 
 "https://secure.runescape.com/m=itemdb_oldschool/Bandos+tassets/viewitem?obj=11834", "https://secure.runescape.com/m=itemdb_oldschool/Primordial+boots/viewitem?obj=13239", 
 "https://secure.runescape.com/m=itemdb_oldschool/Amulet+of+torture/viewitem?obj=19553", "https://secure.runescape.com/m=itemdb_oldschool/Abyssal+whip/viewitem?obj=4151",
@@ -17,6 +13,7 @@ item_urls = ["https://secure.runescape.com/m=itemdb_oldschool/Dragon+hunter+lanc
 rare_urls = ["https://secure.runescape.com/m=itemdb_oldschool/Scythe+of+vitur+%28uncharged%29/viewitem?obj=22486", "https://secure.runescape.com/m=itemdb_oldschool/Twisted+bow/viewitem?obj=20997"]
 total = 0.0
 
+# go to the url for an item and collect information (item name, current price, price change for today, 1 month change, 3 month change, and 6 month change)
 def scrape_ge(url):
     result = requests.get(url)
     soup = bs4.BeautifulSoup(result.text,"lxml")
@@ -24,6 +21,7 @@ def scrape_ge(url):
     prices = [soup.select('span')[4].getText(),soup.select('span')[5].getText(),soup.select('span')[8].getText(),soup.select('span')[11].getText(),soup.select('span')[14].getText()]
     return item,prices,prices[0]
 
+# display item info
 def display_ge_info(item,prices):
     temp = []
     print(item)
@@ -33,6 +31,7 @@ def display_ge_info(item,prices):
     print("3 month change: " + prices[3])
     print("6 month change: " + prices[4])
 
+# display information for all items that I currently own and display the total value at the end
 for i in item_urls:
 	item,prices,cur_price = scrape_ge(i)
 	display_ge_info(item,prices)
@@ -43,6 +42,7 @@ for i in item_urls:
 print("Total value: " + str(total) + "m")
 print()
 
+# display information for high value items or items that I want to track for flipping
 for i in rare_urls:
 	item,prices,cur_price = scrape_ge(i)
 	display_ge_info(item,prices)
