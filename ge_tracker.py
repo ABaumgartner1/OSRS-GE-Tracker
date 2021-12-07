abd57b62735f052ac2d53fb0274d14e81ec9f51a
ABaumgartner1 <63395475+ABaumgartner1@users.noreply.github.com>
2021-12-07 01:17:11 -0800

    Added ge_tracker
    Intital version of my osrs ge web scraper

---
+++ ge_tracker.py
@@ -0,0 +1,49 @@
+# python web scraping notes
+# - pip install requests
+# - pip install lxml
+# - pip install bs4
+
+# osrs ge item scraper
+
+import requests
+import bs4
+
+item_urls = ["https://secure.runescape.com/m=itemdb_oldschool/Dragon+hunter+lance/viewitem?obj=22978", "https://secure.runescape.com/m=itemdb_oldschool/Bandos+chestplate/viewitem?obj=11832", 
+"https://secure.runescape.com/m=itemdb_oldschool/Bandos+tassets/viewitem?obj=11834", "https://secure.runescape.com/m=itemdb_oldschool/Primordial+boots/viewitem?obj=13239", 
+"https://secure.runescape.com/m=itemdb_oldschool/Amulet+of+torture/viewitem?obj=19553", "https://secure.runescape.com/m=itemdb_oldschool/Abyssal+whip/viewitem?obj=4151",
+"https://secure.runescape.com/m=itemdb_oldschool/Armadyl+chestplate/viewitem?obj=11828", "https://secure.runescape.com/m=itemdb_oldschool/Armadyl+chainskirt/viewitem?obj=11830",
+"https://secure.runescape.com/m=itemdb_oldschool/Toxic+blowpipe+%28empty%29/viewitem?obj=12924", "https://secure.runescape.com/m=itemdb_oldschool/Uncharged+toxic+trident/viewitem?obj=12900",
+"https://secure.runescape.com/m=itemdb_oldschool/Necklace+of+anguish/viewitem?obj=19547", "https://secure.runescape.com/m=itemdb_oldschool/Spectral+spirit+shield/viewitem?obj=12821"]
+rare_urls = ["https://secure.runescape.com/m=itemdb_oldschool/Scythe+of+vitur+%28uncharged%29/viewitem?obj=22486", "https://secure.runescape.com/m=itemdb_oldschool/Twisted+bow/viewitem?obj=20997"]
+total = 0.0
+
+def scrape_ge(url):
+    result = requests.get(url)
+    soup = bs4.BeautifulSoup(result.text,"lxml")
+    item = soup.select('h2')[0].getText()
+    prices = [soup.select('span')[4].getText(),soup.select('span')[5].getText(),soup.select('span')[8].getText(),soup.select('span')[11].getText(),soup.select('span')[14].getText()]
+    return item,prices,prices[0]
+
+def display_ge_info(item,prices):
+    temp = []
+    print(item)
+    print("Price: " + prices[0])
+    print("Today's change: " + prices[1])
+    print("1 month change: " + prices[2])
+    print("3 month change: " + prices[3])
+    print("6 month change: " + prices[4])
+
+for i in item_urls:
+	item,prices,cur_price = scrape_ge(i)
+	display_ge_info(item,prices)
+	print()
+	cur_price = cur_price[0:len(cur_price)-1]
+	total = total + float(cur_price)
+
+print("Total value: " + str(total) + "m")
+print()
+
+for i in rare_urls:
+	item,prices,cur_price = scrape_ge(i)
+	display_ge_info(item,prices)
+	print()
